fd0765bfb1284fdafe47aced7d9d6b021da5bc3d
Rigel Ramadhan <rigelramadhan@gmail.com>
2022-04-27 14:19:31 +0700

    Ready for submission

--- app/src/main/java/com/rigelramadhan/storyapp/application/StoryApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.rigelramadhan.storyapp.application
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class StoryApplication: Application() {
-}
